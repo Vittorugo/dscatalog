90b8e36eca16cc5efd614de16d1520ee1eab143f
vittorugo <hgloureirocosta@gmail.com>
2022-12-04 10:42:12 -0300

    Add CategoryDTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.entities.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+
+    public CategoryDTO(Category entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+}
